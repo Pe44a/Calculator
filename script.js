ea1c6b2b00ddccd64d41d2c1332492c90f90424b
Peteris <blankfeldspeteris@gmail.com>
2022-07-25 16:37:54 +0300

    Made function operate()
    This function takes three parameters:
    
    * an operator
    *2 numbers
    
     and then calls one of the functions(add,subtract etc) on the numbers

--- script.js
+++ script.js
@@ -13,3 +13,21 @@ const multiply = function(a,b){
 const divide = function(a,b){
     return a / b;
 }
+
+
+const operate = function(operator,a,b){
+    if(operator === '+'){
+        let result = add(a,b);
+            return result;
+    } else if(operator === '-'){
+        let result = subtract(a,b);
+            return result;
+    } else if(operator === 'x'){
+        let result = multiply(a,b);
+            return result;
+    } else if(operator === ':'){
+        let result = divide(a,b);
+            return result;
+    }
+}
+
