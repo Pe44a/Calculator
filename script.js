8ad56468118f6ee482d3a568316fea90b64029e0
Peteris <blankfeldspeteris@gmail.com>
2022-08-01 14:40:54 +0300

    Made calculator responsive
    Calculator is now able to:
    *Calculate basic math
    *Clear the display and values

--- script.js
+++ script.js
@@ -1,16 +1,9 @@
-const displayValue = document.querySelector('.display');
-const displayText = document.querySelector('#display-text');
+const displayValue = document.querySelector('#display-text');
 const buttons = document.querySelectorAll('button');
-
-
-
-buttons.forEach((button) => {
-    button.addEventListener('click', (event) => {
-        displayText.style.color = 'white';
-        displayText.textContent += event.target.textContent;
-    });
-  });
-
+const numbers = document.querySelectorAll('.number');
+let firstNum = '';//First num in calculator
+let secondNum = '';//Second num in calculator
+let operatorValue = '';//Calculator parameter
 
 
 const add = function(a,b){
@@ -46,3 +39,45 @@ const operate = function(operator,a,b){
     }
 }
 
+const calculate = function() {
+    const result = operate(operatorValue,Number(firstNum),Number(secondNum));
+        displayValue.textContent = result;
+            firstNum = result;
+                secondNum = '';
+}
+
+
+buttons.forEach((button) => {
+    button.addEventListener('click', (event) => {
+        displayValue.style.color = 'white';
+
+        //Checks whether you pressed a number or dot
+        if(event.target.classList.contains('number') && operatorValue === '') {
+            displayValue.textContent += event.target.textContent;
+                firstNum += event.target.textContent;
+        } 
+        //When you press 5-2 and then press + it shows the result of that calculation, 3, it repeats until you press =
+        if(event.target.classList.contains('operator') && firstNum !== '' && secondNum !== '' && operatorValue !== '') {
+            calculate();
+        }
+        //Saves what operator you have pressed
+        if(event.target.classList.contains('operator')) {
+            operatorValue = event.target.textContent;
+        }
+        //Saves secondNum if parameter value is present
+        if(event.target.classList.contains('number') && operatorValue !== '') {
+            secondNum += event.target.textContent;
+                displayValue.textContent = secondNum; 
+        }
+        //Runs operator function if all parameters are present
+        if(event.target.id === 'equal' && firstNum !== '' && secondNum !== '' && operatorValue !== '') {
+            calculate();
+        }
+        if(event.target.id === 'clear') {
+            firstNum = '';
+            secondNum = '';
+            operatorValue = '';
+            displayValue.textContent = '';
+        }
+    });
+  });
