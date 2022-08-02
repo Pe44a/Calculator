7a5d707e23be28ab664b4bc5e085f73196f7e14e
Peteris <blankfeldspeteris@gmail.com>
2022-08-02 13:26:28 +0300

    Fixed the multiple decimal problem
    Decimal(dot) button is disabled if one is already on the display

--- script.js
+++ script.js
@@ -51,23 +51,33 @@ buttons.forEach((button) => {
     button.addEventListener('click', (event) => {
         displayValue.style.color = 'white';
 
+        
         //Checks whether you pressed a number or dot
         if(event.target.classList.contains('number') && operatorValue === '') {
             displayValue.textContent += event.target.textContent;
                 firstNum += event.target.textContent;
+        }
+        //Enables back dot button if there is no dot
+        if(displayValue.textContent.indexOf('.') === -1) {
+            document.getElementById('dot').disabled = false;
         } 
+        //Disables dot button if there is already a decimal number
+        if(displayValue.textContent.indexOf('.') !== -1) {
+            document.getElementById('dot').disabled = true;
+        }
         //When you press 5-2 and then press + it shows the result of that calculation, 3, it repeats until you press =
         if(event.target.classList.contains('operator') && firstNum !== '' && secondNum !== '' && operatorValue !== '') {
             calculate();
         }
-        //Saves what operator you have pressed
+        //Saves operator that you clicked
         if(event.target.classList.contains('operator')) {
             operatorValue = event.target.textContent;
         }
         //Saves secondNum if parameter value is present
         if(event.target.classList.contains('number') && operatorValue !== '') {
             secondNum += event.target.textContent;
-                displayValue.textContent = secondNum; 
+                displayValue.textContent = secondNum;
+                    document.getElementById('dot').disabled = false; 
         }
         //Runs operator function if all parameters are present
         if(event.target.id === 'equal' && firstNum !== '' && secondNum !== '' && operatorValue !== '') {
